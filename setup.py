97c38f682185121ac497c6d29df205a793b72daf
cephox <paul-stier@gmx.de>
2021-03-25 08:54:29 +0100

    Increased project version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ classifiers = [
 
 setuptools.setup(
     name="PyPassMan",
-    version="1.0.0",
+    version="1.0.1",
     description="An encrypted and secure password manager",
     long_description=open("README.md", "r").read(),
     long_description_content_type="text/markdown",
